8811469181e6c3b19f676a596109ba245a0faccf
insorker <2677331029@qq.com>
2023-11-13 15:29:38 +0800

    deploy

---
+++ assets/AboutView-cfcfd0cf.js
@@ -0,0 +1,1 @@
+import{d as e,c as _,j as t,a as o,o as s,_ as a}from"./app-e592db3c.js";const c={class:"about"},d=e({__name:"AboutView",setup(n){return(r,i)=>(s(),_("div",c,[t(" To be built ... "),o(" <UserCard /> ")]))}});const p=a(d,[["__scopeId","data-v-d4948df9"],["__file","/mnt/e/_yori_blog/yori-vue/src/views/AboutView.vue"]]);export{p as default};
