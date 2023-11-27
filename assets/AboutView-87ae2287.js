c9fc08c8d3337f371f9d9802f2bc41e73ffd18fe
insorker <2677331029@qq.com>
2023-11-27 16:48:25 +0800

    update

---
+++ assets/AboutView-87ae2287.js
@@ -0,0 +1,1 @@
+import{d as e,c as _,j as t,a as o,o as s,_ as a}from"./app-6aaf0d91.js";const c={class:"about"},d=e({__name:"AboutView",setup(n){return(r,u)=>(s(),_("div",c,[t(" To be built ... "),o(" <UserCard /> ")]))}});const p=a(d,[["__scopeId","data-v-d4948df9"],["__file","D:/Project/_blog/yori-vue/src/views/AboutView.vue"]]);export{p as default};
